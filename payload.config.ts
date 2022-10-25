f9c9f351f489a9e2f75c8daccabbe75401643f59
Augustorresfx <augustorresfx@gmail.com>
2022-10-25 19:24:27 -0300

    first-time-payloadcms

---
+++ payload.config.ts
@@ -0,0 +1,16 @@
+import { buildConfig } from 'payload/config';
+import dotenv from 'dotenv';
+import Page from './collections/Page';
+import Media from './collections/Media';
+import FormSubmission from './collections/FormSubmission';
+
+dotenv.config();
+
+export default buildConfig({
+  serverURL: process.env.PAYLOAD_PUBLIC_SERVER_URL,
+  collections: [
+    FormSubmission,
+    Page,
+    Media,
+  ],
+});
